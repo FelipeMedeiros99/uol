f12c8a684eb54818dc21b127686b0d5f3ef57192
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2023-10-24 18:39:37 -0300

    mensagens do servidor plotando na tela e o scroll rolando para a última mensagem

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //------------ variáveis --------// 
 const repositorio = 'https://mock-api.driven.com.br/api/v4/uol/participants'
 const statusOnline = "https://mock-api.driven.com.br/api/v4/uol/status"
+const mensagensServidor = "https://mock-api.driven.com.br/api/v4/uol/messages"
+let nomeUsuario = ""
 let main = document.querySelector('main')
 
 
@@ -16,21 +18,23 @@ function mostraErro(erro){
 }
 
 function entrarNaSala(dado){
-    plotaMensagem("entrada", nomeUsuario, "entrou na sala...")
+    let hora = Date().substring(16, 24);
+    // plotaMensagem("status", nomeUsuario, "entrou na sala...")
+    plotaMensagem(tipo="status", usuarioQueEnviou=nomeUsuario, usuarioQueRecebe="", mensagem="entrou na sala...", hora)
+    scrolBaixo()
 }
 
-function plotaMensagem(tipo, usuario, mensagem){
-    let hora = Date().substring(16, 24);
+function plotaMensagem(tipo, usuarioQueEnviou, usuarioQueRecebe="", mensagem, hora=''){
+    // console.log(tipo, usuarioQueEnviou, usuarioQueRecebe, mensagem, hora)
     // tipo: alerta de usuario entrando/saindo, 
     // mensagens gerais e mensagens privadas.
-    if (tipo == 'entrada' || tipo == 'saida'){
+    if (tipo == 'status'){
         main.innerHTML += `
-        <div class=${tipo}>
+        <div class="${tipo} mensagem">
             <p>
-                <span>(${hora})</span>  <strong>${usuario}</strong> ${mensagem}
+                <span>(${hora})</span>  <strong>${usuarioQueEnviou}</strong> ${mensagem}
             </p>
-        </div>
-            `
+        </div>`
     }
 
 }
@@ -39,20 +43,48 @@ function manterOnline(){
     axios.post(statusOnline, {name: nomeUsuario})
 }
 
+function criaTelaDeMensagens(){
+    let promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    promessa.then(recebeDadosMensagens)
+    promessa.catch(mostraErro)
+}
+
+function recebeDadosMensagens(dado){
+    
+    // gerando todas as mensagens e plotando na tela
+    let dadosGerais = dado.data
+    dadosGerais.forEach(function(informacoes){plotaMensagem(
+        tipo=informacoes.type,
+        usuarioQueEnviou=informacoes.from,
+        usuarioQueRecebe=informacoes.to,
+        mensagem=informacoes.text, 
+        hora=informacoes.time)} 
+    )
+
+    // adicionando o novo usuario na conversa
+    novoUsuario()
+}
+
+function novoUsuario(){
+    // pegando dados do usuário
+    nomeUsuario = prompt('Insira seu nome: ')
+    // enviando para o axios e entrando na sala
+    let promessaEnvio = axios.post(repositorio, {name: nomeUsuario})
+    promessaEnvio.then(entrarNaSala)
+    promessaEnvio.catch(mostraErro)
+
+    // ficando online
+    setInterval(manterOnline, 3000)
+    
+}
+
+function scrolBaixo(){
+    let sessaoMensagens = document.querySelectorAll('.mensagem')
+    // let listaHtmlDasMensagens = sessaoMensagens.map(function(elemento){return elemento.innerHTML})
+    sessaoMensagens[sessaoMensagens.length - 1].scrollIntoView()
+    
+    
+}
+
 // ------------ código ----------//
-
-// pegando dados do usuário
-let nomeUsuario = prompt('Insira seu nome: ')
-
-// enviando para o axios e entrando na sala
-let promessaEnvio = axios.post(repositorio, {name: nomeUsuario})
-promessaEnvio.then(entrarNaSala)
-promessaEnvio.catch(mostraErro)
-
-// ficando online
-setInterval(manterOnline, 3000)
-
-
-// let promessa = axios.get(repositorio)
-// promessa.then(mostraRetorno)
-// promessa.catch(mostraErro)
+criaTelaDeMensagens()
