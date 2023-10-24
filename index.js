07da8ae8d813dd4838ce91bd682e1c2815982a5c
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2023-10-24 20:07:03 -0300

    Função de envio ativada

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const statusOnline = "https://mock-api.driven.com.br/api/v4/uol/status"
 const mensagensServidor = "https://mock-api.driven.com.br/api/v4/uol/messages"
 let nomeUsuario = ""
 let main = document.querySelector('main')
-
+incioUsuario = true
+let texto = ""
+let hora = ""
 
 //------------ funçoes ----------//
 function mostraRetorno(dado){
@@ -18,13 +20,13 @@ function mostraErro(erro){
 }
 
 function entrarNaSala(dado){
-    let hora = Date().substring(16, 24);
-    // plotaMensagem("status", nomeUsuario, "entrou na sala...")
+    hora = Date().substring(16, 24);
+    // plotando mensagens
     plotaMensagem(tipo="status", usuarioQueEnviou=nomeUsuario, usuarioQueRecebe="", mensagem="entrou na sala...", hora)
-    scrolBaixo()
+    scrollBaixo()
 }
 
-function plotaMensagem(tipo, usuarioQueEnviou, usuarioQueRecebe="", mensagem, hora=''){
+function plotaMensagem(tipo, usuarioQueEnviou, usuarioQueRecebe="", mensagem, hora=hora){
     // console.log(tipo, usuarioQueEnviou, usuarioQueRecebe, mensagem, hora)
     // tipo: alerta de usuario entrando/saindo, 
     // mensagens gerais e mensagens privadas.
@@ -32,10 +34,19 @@ function plotaMensagem(tipo, usuarioQueEnviou, usuarioQueRecebe="", mensagem, ho
         main.innerHTML += `
         <div class="${tipo} mensagem">
             <p>
-                <span>(${hora})</span>  <strong>${usuarioQueEnviou}</strong> ${mensagem}
+                <span>(${hora})</span>  <strong>${usuarioQueEnviou}</strong>: ${mensagem}
             </p>
         </div>`
+    }else if (tipo == "message"){
+    main.innerHTML += 
+    `<div class="${tipo} mensagem">
+        <p>
+            <span>(${hora})</span>  <strong>${usuarioQueEnviou}</strong> para <strong>${usuarioQueRecebe}</strong>: ${mensagem}
+        </p>
+    </div>`
     }
+
+    main.scrollTop = main.scrollHeight
 
 }
 
@@ -58,11 +69,15 @@ function recebeDadosMensagens(dado){
         usuarioQueEnviou=informacoes.from,
         usuarioQueRecebe=informacoes.to,
         mensagem=informacoes.text, 
-        hora=informacoes.time)} 
+        hora=informacoes.time)
+        } 
     )
+    if (incioUsuario){
+        novoUsuario()
+    }
+}
 
-    // adicionando o novo usuario na conversa
-    novoUsuario()
+function gerandoMensagensNaTela(dado){
 }
 
 function novoUsuario(){
@@ -75,16 +90,45 @@ function novoUsuario(){
 
     // ficando online
     setInterval(manterOnline, 3000)
+    incioUsuario = false
+}
+
+function scrollBaixo(){
+    // modo 1
+    // let sessaoMensagens = document.querySelectorAll('.mensagem')
+    // sessaoMensagens[sessaoMensagens.length - 1].scrollIntoView()
+    main = document.querySelector('main')
+    main.scrollTop = main.scrollHeight;
     
 }
 
-function scrolBaixo(){
-    let sessaoMensagens = document.querySelectorAll('.mensagem')
-    // let listaHtmlDasMensagens = sessaoMensagens.map(function(elemento){return elemento.innerHTML})
-    sessaoMensagens[sessaoMensagens.length - 1].scrollIntoView()
-    
-    
+function enviarMensagem(){
+    texto = document.querySelector('input').value
+    console.log(texto)
+    hora = Date().substring(16, 24);
+    let dadosDoEnvio = {
+        from: nomeUsuario, 
+        to: 'Todos',
+        text: texto,
+        type: 'message',
+        time: hora
+    }
+
+    // limpando a tela
+
+    let promessaEnvio = axios.post(mensagensServidor, dadosDoEnvio)
+    promessaEnvio.then(adicionaMensagemTodos)
+    promessaEnvio.catch(mostraErro)
 }
+
+function adicionaMensagemTodos(dado){
+    plotaMensagem(tipo='message', usuarioQueEnviou=nomeUsuario, usuarioQueRecebe="Todos", mensagem=texto, hora=hora)
+}
+
 
 // ------------ código ----------//
+// setInterval(criaTelaDeMensagens, 3000)
+
+
 criaTelaDeMensagens()
+
